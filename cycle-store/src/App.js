d2b96cb0b2e64f9ad56fc99de5588305f9c5a4d4
Devender Shakya <devshakya@gmail.com>
2023-12-03 23:53:57 +0530

    Half Done

--- cycle-store/src/App.js
+++ cycle-store/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import AllRoutes from './Routes/AlllRoutes';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <AllRoutes/>
+      <Footer /> 
     </div>
   );
 }
